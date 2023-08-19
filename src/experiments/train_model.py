baff46ec365afad7cd23de9e362cc13712b1722b
Maurin Voshol <johannes.voshol@student.uantwerpen.be>
2023-08-19 17:20:24 +0200

    Update train_model.py

--- src/experiments/train_model.py
+++ src/experiments/train_model.py
@@ -1,112 +1,100 @@
 from time import time
 
-import numpy as np
 import torch
+from sklearn import metrics
+from torch.optim import AdamW
+from tqdm import trange
 
-from src.data.temporal_causal_data import TemporalCausalData
-from src.eval.auc_roc import calculate_AUROC
-from src.models.model_outputs import EvaluationResult, TrainResult, ModelOutput
-from src.models.model_config import ModelConfig, TrainConfig
-from src.utils.progress_3 import Nested_trange
+from src.models.NAVAR import NAVAR
+from src.models.TAMCaD import TAMCaD
+
 from src.utils.pytorch import exponential_scheduler_with_warmup
 
 
-def train_model(causal_data: TemporalCausalData,
-                model_config: ModelConfig,
-                train_config: TrainConfig,
-                progress_manager: Nested_trange) -> EvaluationResult:
+def train_NAVAR(train_data: torch.Tensor, lr, epochs, weight_decay, test_size,
+                true_causal_matrix=None, disable_tqdm=False, lambda1=0.2, **kwargs):
+    """
+    data: tensor, is of size (batch_size, n_variables, sequence_length)
+    lr: float, learning rate
+    epochs: int
+    weight_decay: float
+    true_causal_matrix: tensor, is of size (batch_size, n_variables, n_variables)
+    disable_tqdm: bool, turn off while doing hyperopt search
+    """
 
-    dataset = causal_data.timeseries_data
-    # Record start time
-    start_time = time()
+    if true_causal_matrix is not None:
+        true_causal_matrix = true_causal_matrix.flatten()
 
-    # Test the model every 30 epochs, or at the last epoch
-    test_every = 25
-    assert train_config.num_epochs % test_every == 0, 'num_epoch should be a multiple of 25'
-
-    # Instantiate the model
-    model = model_config.instantiate_model()
+    model = NAVAR(n_variables=train_data.size(1), **kwargs)
 
     # Move the model and data to the GPU if it's available
     if torch.cuda.is_available():
         model = model.cuda()
-        dataset = dataset.cuda()
+        train_data = train_data.cuda()
 
-    # Split data into experiments and validation sets
-    if train_config.test_size == 0:
-        train_data, test_data = dataset, dataset
-    else:
-        train_data, test_data = dataset.train_test_split(train_config.test_size)
+    x_test, y_test = None, None
+    if test_size > 0.0:
+        n = int(train_data.size(-1) * test_size)
+        test_data = train_data[..., -n:]
+        train_data = train_data[..., :-n]
+        x_test, y_test = test_data[..., :-1], test_data[..., 1:]
+    x_train, y_train = train_data[..., :-1], train_data[..., 1:]
 
     # Define the optimizer
-    optimizer = train_config.optimizer(model.parameters(),
-                                       lr=train_config.learning_rate,
-                                       weight_decay=train_config.weight_decay)
-    scheduler = exponential_scheduler_with_warmup(optimizer, start_factor=0.01, end_factor=0.01,
-                                                  warmup_iters=100, exp_iters=train_config.num_epochs // 2,
-                                                  total_iters=train_config.num_epochs)
+    optimizer = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    scheduler = exponential_scheduler_with_warmup(optimizer,
+                                                  start_factor=0.05, end_factor=0.05,
+                                                  warmup_iters=int(epochs * 0.05),
+                                                  cooldown_iters=int(epochs * 0.2),
+                                                  total_iters=epochs)
 
-    # Define lists to store the experiments and validation losses
-    train_losses, test_losses, train_losses_true, test_losses_true = [], [], [], []
+    train_losses, test_losses, train_auc, test_auc, train_cm, test_cm = [], [], [], [], [], []
 
-    # AUCROC
-    auroc_scores = []
-    true_causal_matrix = causal_data.causal_graph.get_causal_matrix(exclude_max_lags=True)
+    start_time = time()
 
-    # Set the model to experiments mode
     model.train()
 
-    # Train the model
-    for epoch in progress_manager.iter(loop_index=2):
-        # Forward pass and loss calculation
-        model_output: ModelOutput = model(train_data.train_data)
+    progressbar = trange(epochs, disable=disable_tqdm)
+    for _ in progressbar:
+        loss, auc, cm = navar_epoch(model, x_train, y_train, lambda1, true_causal_matrix)
 
-        # Backward pass and optimization
-        model_output.get_loss().backward()
+        train_losses.append(loss.item())
+        train_cm.append(cm)
+        if true_causal_matrix is not None:
+            train_auc.append(auc)
+
+        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         scheduler.step()
 
-        # Store the experiments loss
-        train_losses.append(model_output.get_regression_loss().item())
+        desc = "train_loss={:.3f}, train_auc={:.2f}".format(train_losses[-1], train_auc[-1])
 
-        # Test the model on the validation set
-        if epoch % test_every == 0 or epoch == train_config.num_epochs - 1:
+        if test_size > 0.0:
             with torch.no_grad():
-                model_output = model.forward_eval(test_data.train_data)
-                test_losses.append(model_output.get_regression_loss().item())
-                train_losses_true.append(model.forward_eval(train_data.train_data, train_data.true_data).get_regression_loss().item())
-                test_losses_true.append(model.forward_eval(test_data.train_data, test_data.true_data).get_regression_loss().item())
-                cm_size = model_output.causal_matrix.size()
-                auroc_scores.append({
-                    'epoch': epoch,
-                    **calculate_AUROC(model_output.causal_matrix, true_causal_matrix[:cm_size[0], :cm_size[1]])
-                })
-                progress_manager.set_info("train={:.4f}, test={:.4f}, AUCROC={:.3f}".format(
-                    train_losses[-1], test_losses[-1], auroc_scores[-1]['score']), refresh=False)
+                loss, auc, cm = navar_epoch(model, x_test, y_test, lambda1, true_causal_matrix)
 
-                if epoch > train_config.num_epochs // 2 and auroc_scores[-1]['score'] < 0.5:
-                    break
+                test_losses.append(loss.item())
+                test_cm.append(cm)
+                if true_causal_matrix is not None:
+                    test_auc.append(auc)
+                desc += "test_loss={:.3f}, test_auc={:.2f}".format(test_losses[-1], test_auc[-1])
 
-    # Evaluate the model on the experiments data
-    with torch.no_grad():
-        if model.use_variational_layer:
-            model_result = model.forward_eval(dataset.train_data, dataset.true_data).cpu()
-        else:
-            model_result = model.monte_carlo(dataset.train_data, dataset.true_data, samples=200, max_batch_size=25).cpu()
+        progressbar.set_description(desc, refresh=False)
 
-    train_result = TrainResult(model,
-                               train_losses, test_losses,
-                               train_losses_true, test_losses_true,
-                               auroc_scores,
-                               test_every,
-                               time() - start_time)
+    return {
+        ...
+    }
 
-    # Return the evaluation result object
-    return EvaluationResult(
-        dataset=causal_data,
-        model_config=model_config,
-        model_output=model_result,
-        train_config=train_config,
-        train_result=train_result
-    )
+
+def navar_epoch(model, x, y, lambda1, true_causal_matrix_flat=None):
+    output = model(x)
+    loss = model.loss_function(y, *output, lambda1=lambda1)
+    contributions = output[1]
+    causal_matrix = contributions.detach().std(dim=-1).cpu().numpy()  # (batch_size, n_variables, n_variables)
+    if true_causal_matrix_flat is not None:
+        fpr, tpr, _ = metrics.roc_curve(true_causal_matrix_flat, causal_matrix.flatten())
+        auc = metrics.auc(fpr, tpr)
+    else:
+        auc = None
+    return loss, auc, causal_matrix
