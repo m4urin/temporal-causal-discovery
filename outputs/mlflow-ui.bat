1a0edec2128cde56224b8e4b651aec12c12508d5
Maurin Voshol <johannes.voshol@student.uantwerpen.be>
2024-01-26 22:03:06 +0100

    refactor

--- outputs/mlflow-ui.bat
+++
@@ -1,5 +0,0 @@
-call %UserProfile%\Anaconda3\Scripts\activate.bat
-call activate causal-discovery
-start mlflow ui
-timeout /t 1
-start http://127.0.0.1:5000
--- outputs/mlflow-ui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xdg-open http://127.0.0.1:5000
-conda activate causal-discovery
-mlflow ui
